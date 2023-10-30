bf60a1f8cdca491a1b1d05426fe407d4e3491cd1
Mirko Gilioli <mirko.gilioli@gmail.com>
2023-10-30 12:25:44 +0100

    Templating lab 12

--- models/marts/obt_reporting.sql
+++ models/marts/obt_reporting.sql
@@ -1,7 +1,9 @@
 {{
     config (
         materialized="incremental",
-        unique_key="<PURCHASE ID PRIMARY KEY IN OBT TABLE>"
+        unique_key="purchaseid",
+        partition_by={"field":"<PARTITION FIELD>", "data_type":"<FIELD DATA TYPE>"},
+        incremental_strategy="<INCREMENTAL STRATEGY NAME>"
     )
 }}
 
@@ -19,15 +21,13 @@ select
 from 
     <TMP OBT TABLE NAME>
 {%if is_incremental()%}
-where insertat > (select <BIGQUERY TIMESTAMP SUB FUNCTION>(max(insertat), interval <N OF DAYS> day) from {{this}}) 
+where insertat > (select timestamp_sub(max(insertat), interval 3 day) from {{this}}) 
 {%endif%}
 
 /*
-Use the following query in BigQuery to insert two records into the source table. Use for testing purpose
+Use the following query in BigQuery to insert one records into the source table. Use for testing purpose
 
 INSERT INTO `<YOUR GC PROJECT ID>.<RAW DATASET ID>.purchases` (_etl_loaded_at, customer_id, purchase_date, purchase_id, purchase_type, quantity, sky_pass_id) 
 VALUES (CURRENT_TIMESTAMP(), 1, CURRENT_TIMESTAMP(), <NEW PURCHASE ID>, 'credit_card', 1, 3);
 
-INSERT INTO `<YOUR GC PROJECT ID>.<RAW DATASET ID>.purchases` (_etl_loaded_at, customer_id, purchase_date, purchase_id, purchase_type, quantity, sky_pass_id) 
-VALUES (TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 DAY), 1, CURRENT_TIMESTAMP(), <NEW PURCHASE ID>, 'credit_card', 1, 3)
 */
